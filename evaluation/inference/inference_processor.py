e227a1f097e5c0c395d0f3cfa99a6d8d72088173
Maksim Eremeev <maks5507@yandex.ru>
2021-04-21 13:23:29 -0400

    some evaluation scripts

---
+++ evaluation/inference/inference_processor.py
@@ -0,0 +1,33 @@
+#
+# Created by Maksim Eremeev (mae9785@nyu.edu)
+#
+
+from elsa import Elsa
+from pathlib import Path
+import os
+
+
+class InferenceProcessor:
+    def __init__(self, log, save_path, elsa_params):
+        self.log = log
+        self.save_path = save_path
+        self.elsa = Elsa(**elsa_params)
+
+    def run(self, path, model_params):
+        with open(path, 'r') as f:
+            user_text = f.read()
+
+        self.log.info(f'{path}')
+        basename = str(Path(path).stem)
+
+        output_path = f'{self.save_path}/{basename}.summary'
+
+        if os.path.exists(output_path):
+            return
+
+        summary = self.elsa.generate(user_text, **model_params)
+        self.log.info(f'Summary for input {basename}: {summary}')
+
+        with open(output_path, 'w') as f:
+            f.write(summary)
+
